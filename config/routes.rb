e0bb298b5b5ecfd594a82fc0dcf833d29c2ba238
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-04-20 12:14:02 +0200

    added lewagon stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'new/create'
+
+  get 'new/destroy'
+
+  get 'ingredients/index'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
